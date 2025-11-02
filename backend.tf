ff0ea6acc083bd235c36a67a7eee5368a8ce587b
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-11-02 16:31:58 +0100

    Refactor Terraform workflow and backend configuration for improved clarity and functionality

--- backend.tf
+++ backend.tf
@@ -1,11 +1,3 @@
 terraform {
-
-  backend "azurerm" {
-    resource_group_name  = "rg-kolad-sch"
-    subscription_id      = "18a54fef-d36d-4d1c-b7b4-c468a773149b"
-    storage_account_name = "sttfwsslabsec001"
-    container_name       = "tfstate"
-    key                  = "loadbalancer.prod.tfstate"
-    use_oidc             = true
-  }
+  backend "azurerm" {}
 }
