26abf44370121678f529eb3c182e77f7a21ced8a
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-11-09 17:57:31 +0100

    remove cert

--- main.tf
+++ main.tf
@@ -120,15 +120,17 @@ resource "azurerm_key_vault_secret" "vm_admin_password" {
 # KEY VAULT Certificates
 # ============================================================================
 
-resource "azurerm_key_vault_certificate" "vm_mngmnt_cert" {
+/*resource "azurerm_key_vault_certificate" "vm_mngmnt_cert" {
   name         = "imported-cert"
   key_vault_id = azurerm_key_vault.vm_credentials.id
 
-  //certificate {
-    //contents = filebase64("certificate-to-import.pfx")
-    //password = ""
- // }
+  certificate {
+    ontents = filebase64("certificate-to-import.pfx")
+   password = ""
 }
+}*/
+
+
 # ============================================================================
 # APPLICATION SECURITY GROUPS
 # ============================================================================
